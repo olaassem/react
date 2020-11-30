79b04c31f971953f351869b566183d6b4fe700a6
Ola Assem <ola.assem@viget.com>
2020-11-30 17:22:05 +0200

    initial create react app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi. This is a React App.</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
