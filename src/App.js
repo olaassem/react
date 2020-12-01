588d6100595d4f84778eb0348830ad9390622b25
Ola Assem <ola.assem@viget.com>
2020-12-01 09:54:14 +0200

    set state w class components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
+
+  state = {
+    persons : [
+      { name: 'One', age: 1 },
+      { name: 'Two', age: 2 },
+      { name: 'Three', age: 3 },
+    ]
+  }
+
+  switchNameHandler = () => {
+    this.setState({
+      persons : [
+        { name: 'One Million', age: 1 },
+        { name: 'Two', age: 2 },
+        { name: 'Three', age: 3000 },
+      ]
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi. This is a React App.</h1>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}> I am first</Person>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}> I am second</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}> I am second</Person>
       </div>
     );
   }
