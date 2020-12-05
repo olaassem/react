fb95c73cf73ed9210c745bc1fd1167b82a7ada86
Ola Assem <ola.assem@viget.com>
2020-12-05 20:39:06 +0200

    css modules & media queries

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,25 +1,11 @@
 import React from 'react';
-import './Person.css';
-import styled from 'styled-components';
 
+import classes from './Person.css';
 
-const StyledDiv = styled.div `
-    width: 60%;
-    margin: 16px auto;
-    border: 1px solid #eee;
-    box-shadow: 0 2px 3px #ccc;
-    padding: 16px;
-    text-align: center;
-
-    @media (min-width: 500px): {
-        width: '450px'
-`
 
 const person = (props) => {
-
-
     return (
-        <StyledDiv>
+        <div className={classes.Person}>
             <input 
                 type="text" 
                 onChange={props.changed} 
@@ -28,7 +14,7 @@ const person = (props) => {
             <p>I am {props.name} and {props.age} years old.</p>
             <button onClick={props.click}>Delete Person</button>
             <p>{props.children}</p>
-        </StyledDiv>
+        </div>
     )
 };
 
