575d2140bde4a8a9bf01edad85e0204975a0dfe6
Ola Assem <ola.assem@viget.com>
2020-12-10 15:52:50 +0200

    update lifecycle for prop changes

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,19 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Person from './Person/Person';
 
-const persons = (props) =>  {
-  console.log('[Persons.js] rendering...');
-  return  props.persons.map( ( person, index ) => {      
-    return (
-      <Person
-        click={() => props.clicked(index)} 
-        name={person.name} 
-        age={person.age} 
-        key={person.id}
-        changed={(e) => props.changed(e, person.id)}
-      />
-    )
-  });
-};
+class Persons extends Component {
 
-export default persons;
+  // static getDerivedStateFromComponent(props, state) {
+  //   console.log('[Persons.js] getDerivedStateFromProps');
+  //   return state;
+  // }
+
+  shouldComponentUpdate(nextProps, nextState){
+    console.log('[Persons.js] shouldComponentUpdate');
+    return true;
+  }
+
+  getSnapshotBeforeUpdate(prevProps, prevState){
+    console.log('[Persons.js] getSnapshotBeforeUpdate');
+    return null;
+  }
+
+  componentDidUpdate(){
+    console.log('[Persons.js] componentDidUpdate');
+  }
+
+  render() {
+    console.log('[Persons.js] rendering...');
+    return this.props.persons.map( ( person, index ) => {       
+      return (
+        <Person
+          click={() => this.props.clicked(index)} 
+          name={person.name} 
+          age={person.age} 
+          key={person.id}
+          changed={(e) => this.props.changed(e, person.id)}
+        />
+      );
+    });
+  }
+}
+
+export default Persons;
