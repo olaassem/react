0cb586f3287e0765f44689ce7da953e897b7e401
Ola Assem <ola.assem@viget.com>
2020-12-13 13:12:07 +0200

    aux component for adjacent jsx elements

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 
+import Aux from '../../../hoc/Aux';
 import classes from './Person.css';
 
 
@@ -8,16 +9,18 @@ class Person extends Component {
     
         console.log('[Person.js] rendering...');
 
-        return [
-            <input 
-                key="i1"
-                type="text" 
-                onChange={this.props.changed} 
-                value={this.props.name} />,
-            <p key="i2">I am {this.props.name} and {this.props.age} years old.</p>,
-            <button key="i3" onClick={this.props.click}>Delete Person</button>,
-            <p key="i4">{this.props.children}</p>
-        ]
+        return (
+            <Aux>
+                <input 
+                    key="i1"
+                    type="text" 
+                    onChange={this.props.changed} 
+                    value={this.props.name} />
+                <p key="i2">I am {this.props.name} and {this.props.age} years old.</p>
+                <button key="i3" onClick={this.props.click}>Delete Person</button>
+                <p key="i4">{this.props.children}</p>
+            </Aux>
+        )
     }
 };
 
