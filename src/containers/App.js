894df435023850d8c61b08b5b9406d3c073a4d2b
Ola Assem <ola.assem@viget.com>
2020-12-14 19:51:52 +0200

    sets state correctly for changedCounter

--- src/containers/App.js
+++ src/containers/App.js
@@ -18,7 +18,8 @@ class App extends Component {
       { id:'1gree', name: 'Two', age: 2 },
       { id:'19iuj', name: 'Three', age: 3 },
     ],
-    showPersons: false
+    showPersons: false,
+    changeCounter: 0
   }
 
   static getDerivedStateFromProps(props, state) {
@@ -56,7 +57,12 @@ class App extends Component {
     const persons = [...this.state.persons];
     persons[personIndex] = person;
 
-    this.setState({ persons: persons });
+    this.setState( (prevState, props) => {
+      return {
+        persons: persons,
+        changeCounter: prevState.changeCounter + 1
+      };
+    });
   }
 
 
