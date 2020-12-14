89fe7dc01359d9b08cd2e65b2824d97c8cda574e
Ola Assem <ola.assem@viget.com>
2020-12-14 20:17:53 +0200

    updated way of using refs with createRef

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -8,8 +8,14 @@ import classes from './Person.css';
 
 
 class Person extends Component {
+    constructor(props) {
+        super(props);
+        this.inputElementRef = React.createRef();
+    }
+
     componentDidMount() {
-        this.inputElement.focus();
+        // this.inputElement.focus();
+        this.inputElementRef.current.focus();
     }
     
     render() {
@@ -19,7 +25,8 @@ class Person extends Component {
             <Aux>
                 <input 
                     key="i1"
-                    ref={(inputEl) => {this.inputElement = inputEl}}
+                    // ref={(inputEl) => {this.inputElement = inputEl}}
+                    ref={this.inputElementRef}
                     type="text" 
                     onChange={this.props.changed} 
                     value={this.props.name} />
